4f52e203c9b19d08f52765d8b55b321389a0b619
Yakov Chaika <thekit3@yandex.ru>
2023-07-04 11:41:47 +0300

    test

--- sandbox.py
+++ sandbox.py
@@ -1,10 +1,4 @@
-wake_up = int(input('Во сколько проснулся: '))
-water = 0
-calories_sum = 0
-
-for hour in range(wake_up, 23, 3):
-    water += 1
-    water('Пошли есть в', hour, 'часов')
-    calories = int(input('Сколько съел?'))
-    calories_sum += calories
-print('Выпито литров воды:' water )
+n = int(input('Введите колличество секунд: '))
+for sec in range(n, 0, -2):
+    print(sec)
+print('Я иду искать!')
