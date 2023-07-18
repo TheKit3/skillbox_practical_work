e3994c14ad647d3ad6fa3fcf3154aff06aef7566
TheKit3 <thekit3@yandex.ru>
2023-07-18 19:15:20 +0300

    8 module - finished

--- mod9/task5.py
+++ mod9/task5.py
@@ -8,9 +8,8 @@ for symbol in text:
         symbol_counter += 1    # Пока у нас нет пробела - считаем символы
 
     else:
-
         if big_word < symbol_counter:    # тут сравниваем значения, и наибольшее записываем в big_word
             big_word = symbol_counter
         symbol_counter = 0    # И обнуляем нашу переменную под счетчтик
 
-print(symbol_counter)
+print(big_word)
---
+++ mod9/task7.py
@@ -0,0 +1,4 @@
+encrypted_word = input('Введите зашифрованное слово: ')
+
+for symbol in encrypted_word:
+    original_word +=
