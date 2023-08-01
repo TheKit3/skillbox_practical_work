9818620f6293dc38037b0204c8cba473e9c8d822
Yakov Chaika <thekit3@yandex.ru>
2023-08-01 15:54:57 +0300

    mod10 4 task - piz....

--- mod10/task4.py
+++ mod10/task4.py
@@ -1,16 +1,23 @@
+# Простое число делиться только на 1 и на себя (имеет 2 делителя).
+
 qDigits = int(input('Введите количество цифр: '))
-prime_numbers = 0
-dividers = 0
+prime_numbers = 0    # количесто простых чисел
+dividers = 0    # количество целочисленных делителей
 
 for num in range(qDigits):
     number = int(input("Введите число: "))
-    for N in range(1, number):
+
+    for N in range(1, number + 1):
+        print("\nделим на", N)
+        print("остаток от деления", number % N)
         if number % N == 0:
             dividers += 1
-            if dividers > 2:
-                dividers = 0
-            else:
-                prime_numbers += 1
-                dividers = 0
+            print("количество делителей", dividers)
 
-print(prime_numbers)
+        if dividers == 2:
+            prime_numbers += 1
+
+        dividers = 0
+    print(prime_numbers)
+
+print("Простых чисел:", prime_numbers)
--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,23 @@
-N = int(input("Введите число: "))
+# Простое число делиться только на 1 и на себя (имеет 2 делителя).
 
-for row in range(N):
-    for col in range(row, N):
-        print(col, end="")
-    print()
+qDigits = int(input('Введите количество цифр: '))
+prime_numbers = 0    # количесто простых чисел
+dividers = 0    # количество целочисленных делителей
+
+for num in range(qDigits):
+    number = int(input("Введите число: "))
+
+    for N in range(1, number + 1):
+        print("\nделим на", N)
+        print("остаток от деления", number % N)
+        if number % N == 0:
+            dividers += 1
+            print("количество делителей", dividers)
+
+        if dividers == 2:
+            prime_numbers += 1
+
+        dividers = 0
+    print(prime_numbers)
+
+print("Простых чисел:", prime_numbers)
