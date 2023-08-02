85781f94c4aa45fe91da03e36ed4d534c485d3c0
Yakov Chaika <thekit3@yandex.ru>
2023-08-02 17:57:49 +0300

    4 finito, 5 started

--- mod10/task4.py
+++ mod10/task4.py
@@ -1,23 +1,21 @@
+print('Задача 4. Простые числа')
+
 # Простое число делиться только на 1 и на себя (имеет 2 делителя).
 
-qDigits = int(input('Введите количество цифр: '))
-prime_numbers = 0    # количесто простых чисел
-dividers = 0    # количество целочисленных делителей
+qDigits = int(input("Введите количество цифр: "))
+prime_numbers = 0  # количесто простых чисел
+dividers = 0  # количество целочисленных делителей
 
-for num in range(qDigits):
+for num in range(qDigits):  # цикл для перебора чисел
     number = int(input("Введите число: "))
 
-    for N in range(1, number + 1):
-        print("\nделим на", N)
-        print("остаток от деления", number % N)
-        if number % N == 0:
-            dividers += 1
-            print("количество делителей", dividers)
+    for N in range(1, number + 1):  # берем число из цикла
+        if number % N == 0:  # если оно делится без остатка
+            dividers += 1  # увеличиваем количество делителей
 
-        if dividers == 2:
-            prime_numbers += 1
+    if dividers == 2:  # Если количество делителей у числа рано двум
+        prime_numbers += 1  # Увеличиваем количество простых чисел в последовательности
 
-        dividers = 0
-    print(prime_numbers)
+    dividers = 0  # сбрасываем счетчик перед новым проходм
 
-print("Простых чисел:", prime_numbers)
+print("Количество простых чисел в последовательности:", prime_numbers)
---
+++ mod10/task5.py
@@ -0,0 +1,11 @@
+print('Задача 5. Наибольшая сумма цифр')
+
+amount = int(input("ВВедите количетсво чисел: "))
+
+
+
+for num in range(amount):
+    number = input('Введите число: ')
+
+    for N in number:
+
