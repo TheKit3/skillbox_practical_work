d82965870c21b1acb7af244ed545c9d43e500132
Yakov Chaika <thekit3@yandex.ru>
2023-08-03 17:10:52 +0300

    4 finito, 5 started

--- mod10/task1.py
+++ mod10/task1.py
@@ -11,4 +11,3 @@ for row in range(6):
     for col in range(0, 11, 2):
         print("\t", col + row, end="")
     print()
-    
--- mod10/task5.py
+++ mod10/task5.py
@@ -1,11 +1,24 @@
-print('Задача 5. Наибольшая сумма цифр')
+print("Задача 5. Наибольшая сумма цифр")
 
 amount = int(input("ВВедите количетсво чисел: "))
 
+num_counter = 0  # Счетчик суммы цифр
+big_num_counter = 0  # Наибольшая сумма цифр
+remember_num = 0  # Запомним
+big_num = 0  # Наибольшее число 2
 
+for numbers in range(amount):
+    number = int(input("Введите число: "))
+    remember_num = number    # сохраняем в переменную для того чтобы не потерять наше число
 
-for num in range(amount):
-    number = input('Введите число: ')
+    while number != 0:    # считаем и отбрасываем знаки
+        num_counter += number % 10
+        number //= 10
 
-    for N in number:
+    if num_counter > big_num_counter:  # Если счетчик больше, чем наибольшая сумма
+        big_num_counter = num_counter  # счетчик -> наибольшей суммой
+        big_num = remember_num  # изначальное число -> наибольшее
 
+    num_counter = 0  # сбрасываем счетчик
+
+print("Наибольшее число:", big_num, "\nСумма его чисел:", big_num_counter)
