05f6dbafffb845577adc9fa324c6588d7adec8eb
Yakov Chaika <thekit3@yandex.ru>
2023-08-04 17:44:46 +0300

    dadaya

--- mod10/task6.py
+++ mod10/task6.py
@@ -1,2 +1,9 @@
 print('Задача 6. Пирамидка')
 
+height = int(input("Ввведите высоту треугольника: "))
+symbol = "#"
+
+for col in range(height):    # ну тут что-то слишком просто
+    print(str(" " * height) + symbol + (" " * height))
+
+    symbol += "##"
---
+++ mod10/task7.py
@@ -0,0 +1,15 @@
+print('Задача 7. Пирамидка 2')
+
+height = int(input("Ввведите высоту треугольника: "))
+symbol = 1
+
+for row in range()
+
+    for col in range(height):
+
+        print(" " * height))
+        print(symbol)
+        print(" " * height))
+
+        symbol += 2
+        height -= 1
