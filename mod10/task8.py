72f7ca9b5329f3d9b2a026501eb8583224a41ffe
TheKit3 <thekit3@yandex.ru>
2023-09-03 22:37:31 +0300

    попытка

--- mod10/task8.py
+++ mod10/task8.py
@@ -1,18 +1,19 @@
 deep = int(input('Введите число: '))
 K = deep
 
-for row in range(deep, 0, -1):
+for row in range(1, deep + 1):
 
     # Печатаем убывающую последовательность чисел
     for Col in range(deep, deep - row, -1):
         print(Col, end="")
 
     # Заполняем промежуток точками
-    for dots in range(2 * row - 2):
+    for dots in range(K * 2 - 2):
         print(".", end="")
 
     # Печатаем возрастающую последовательность чисел
     for coL in range(deep - row + 1, deep + 1):
         print(coL, end="")
 
+    K -= 1    # Методом тыка пришел к этому :)
     print()
