53c0d5f30cfaadebce785fdfb9c639a4c1bcc4d0
TheKit3 <thekit3@yandex.ru>
2023-09-04 15:34:41 +0300

    finished

--- mod10/task7.py
+++ mod10/task7.py
@@ -1,18 +1,15 @@
-print('Задача 7. Пирамидка 2')
+print("Задача 7. Пирамидка 2")
 
 height = int(input("Введите высоту треугольника: "))
 
-max_width = height * 2 - 1
+start_num = 1
 
 for row in range(1, height + 1):
-    start_number = 1 + (row - 1) * 2
-    row_width = row * 2 - 1
-    padding = (max_width - row_width) // 2
-
-    print(" " * padding, end="")
+    print("\t" * (height - row), end="")
 
     for col in range(row):
-        print(start_number, end=" ")
-        start_number += 2
+        print(start_num, end=" ")
+        start_num += 2
+        print("\t" * 2, end="")
 
     print()
--- mod10/task8.py
+++ mod10/task8.py
@@ -1,8 +1,7 @@
-deep = int(input('Введите число: '))
+deep = int(input("Введите число: "))
 K = deep
 
 for row in range(1, deep + 1):
-
     # Печатаем убывающую последовательность чисел
     for Col in range(deep, deep - row, -1):
         print(Col, end="")
@@ -15,5 +14,5 @@ for row in range(1, deep + 1):
     for coL in range(deep - row + 1, deep + 1):
         print(coL, end="")
 
-    K -= 1    # Методом тыка пришел к этому :)
+    K -= 1  # Методом тыка пришел к этому :)
     print()
