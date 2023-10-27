4974015657717e9d761a3d0d8ea872c9f9af5a7a
Yakov <106113934+TheKit3@users.noreply.github.com>
2023-10-27 13:31:35 +0300

    Update task3.py

--- mod11/task3.py
+++ mod11/task3.py
@@ -1,5 +1,7 @@
 print('Задача 3. Аналог Steam')
 
+import time
+
 file_size = int(input("Введите размер файла в Мб: "))
 net_speed = int(input("Введите скорость вашего соединения в Мб/с: "))
 
@@ -11,6 +13,6 @@ print(time)
 for i in range(1, time+1):
     perсent = int(net_speed * i / file_size * 100)
     print("Прошло", i, "секунд. Скачано", net_speed * i, "из", file_size, "Мб", "(" + str(perсent) + "%)")
-
+    time.sleep(1)
 if file_size % net_speed != 0:
     print("Прошло", i + 1, "секунд. Скачано", file_size, "из", file_size, "Мб (100%)")
