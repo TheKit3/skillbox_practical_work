2291119e751c4c19ca8b5f4693ab5eaccb18d337
s.svyatenko <thekit3@yandex.ru>
2023-11-28 11:14:09 +0300

    6 end

--- mod11/task6.py
+++ mod11/task6.py
@@ -1,56 +1,31 @@
-!
 import math
 
-error_message = "Неверный формат данных"
+def value_restriction(z):
+    while True:
+        try:
+            z = float(input("Введите значение: "))
+            if 0.0 <= z <= 8.0:
+                return(z)
+            else:
+                print("Вы вышли за пределелы доски, попробуйте другое значение.")
+        except ValueError:
+            print("Вы ввели недопустимое значение: ")
 
-while True:
-    try:
-        start_position_x = float(input("Введите положение коня на доске: "))
-        if start_position_x >= 0.0 and start_position_x <= 8.0:
-            break
-        else:
-            print()
-            print("Вы вышли за границу доски. Введите другое значение")
-    except ValueError:
-        print()
-        print(error_message)
+x_start = float()
+y_start = float()
+x_end = float()
+y_end = float()
 
-while True:
-    try:
-        start_position_y = float(input("Введите положение коня на доске: "))
-        if start_position_y >= 0.0 and start_position_y <= 8.0:
-            break
-        else:
-            print()
-            print("Вы вышли за границу доски. Введите другое значение")
-    except ValueError:
-        print()
-        print(error_message)
+x_start_position = value_restriction(x_start)
+y_start_position = value_restriction(y_start)
+x_end_position = value_restriction(x_end)
+y_end_position = value_restriction(y_end)
 
-while True:
-    try:
-        finish_position_x = float(input("Введите позицию на которую вы хотите переместить коня"))
-        if finish_position_x >= 0.0 and finish_position_x <= 8.0:
-            break
-        else:
-            print()
-            print("Вы вышли за границу доски. Введите другое значение")
-    except ValueError:
-        print()
-        print(error_message)
-while True:
-    try:
-        finish_position_y = float(input("Введите позицию на которую вы хотите переместить коня"))
-        if finish_position_y >= 0.0 and finish_position_y <= 8.0:
-            break
-        else:
-            print()
-            print("Вы вышли за границу доски. Введите другое значение")
-    except ValueError:
-        print()
-        print(error_message)
+# вычислим разницу
+x_movement_difference = abs(int(x_start_position * 10) - int(x_end_position * 10))
+y_movement_difference = abs(int(y_start_position * 10) - int(y_end_position * 10))
 
-print(start_position_x)
-print(start_position_y)
-print(finish_position_x)
-print(finish_position_y)
+if x_movement_difference == 2 and y_movement_difference == 2:
+    print("Ход возможен.")
+else:
+    print("Ход невозможен!")
