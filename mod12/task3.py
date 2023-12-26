6b252f2e5e2774ab65bb0741ac6eac99bab1c2ee
TheKit3 <thekit3@yandex.ru>
2023-12-26 08:05:52 +0300

    finito

--- mod12/task3.py
+++ mod12/task3.py
@@ -1,22 +1,51 @@
-# тупее задания я не видел
-# b
-
 def calculator():
+
     NumberInQuestion = int(input("Введите число: "))
-    print("Деёствие с числом: ")
+
+    print("Действие с числом: ")
     print("1 - Вывести сумму его чисел.")
     print("2 - Максимальную цифру.")
     print("3 - Минимальную цифру.")
+
     choice = int(input())
     if choice == 1:
-        SumOfNumbers()
+        SumOfDigits(NumberInQuestion)
+
     elif choice == 2:
-        min
+        maximum_figure(NumberInQuestion)
+
     elif choice == 3:
+        minimum_figure(NumberInQuestion)
 
     else:
         print("Ошибка вввода. Выберите доступный вариант (1, 2, 3)")
 
+    print()
     calculator()
+
+def SumOfDigits(x):
+    sum_result = 0
+    while x > 0:
+        sum_result += x % 10
+        x //= 10
+    print(sum_result)
+
+def maximum_figure(x):
+    number_str = str(x)
+    max_digit = 0
+    for digit in number_str:
+        current_digit = int(digit)
+        if current_digit > max_digit:
+            max_digit = current_digit
+    print("Максимальная цифра числа равна", max_digit)
+def minimum_figure(x):
+    number_str = str(x)
+    min_digit = 9
+    for digit in number_str:
+        current_digit = int(digit)
+        if current_digit < min_digit:
+            min_digit = current_digit
+
+    print("Минимальная цифра числа равна", min_digit)
 
 calculator()
