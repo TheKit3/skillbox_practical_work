1ce891fc659b78dddbd6ec0f52684ec8f3812a7b
TheKit3 <thekit3@yandex.ru>
2023-12-27 08:36:12 +0300

    finito

--- mod12/task4.py
+++ mod12/task4.py
@@ -1,1 +1,14 @@
-def reverse_num():
+def reverse():
+    normal_number = input("Введите число: ")
+
+    reversed_number = normal_number[::-1]
+    print(int(reversed_number))
+    if normal_number.startswith("0"):
+        print("Ноль, который мы убрали, называется ведущим.")
+    reverse()
+
+
+reverse()
+
+
+# добавить выход из функции при условии одного нуля при вводе
